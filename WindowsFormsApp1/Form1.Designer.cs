581eed2b4bd696737f3cff271e6a9de25ce930a1
KomodoHype13 <karimabdelhamid13@gmail.com>
2025-04-25 18:56:10 +0300

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.LaunchButton = new System.Windows.Forms.Button();
+            this.fileButton = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.cacheButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // LaunchButton
+            // 
+            this.LaunchButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.LaunchButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.LaunchButton.FlatAppearance.BorderSize = 2;
+            this.LaunchButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Firebrick;
+            this.LaunchButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Firebrick;
+            this.LaunchButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.LaunchButton.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LaunchButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.LaunchButton.Location = new System.Drawing.Point(442, 332);
+            this.LaunchButton.Name = "LaunchButton";
+            this.LaunchButton.Size = new System.Drawing.Size(134, 63);
+            this.LaunchButton.TabIndex = 1;
+            this.LaunchButton.Text = "Launch without clearing";
+            this.LaunchButton.UseVisualStyleBackColor = false;
+            this.LaunchButton.Click += new System.EventHandler(this.LaunchButton_Click);
+            // 
+            // fileButton
+            // 
+            this.fileButton.Location = new System.Drawing.Point(589, 245);
+            this.fileButton.Name = "fileButton";
+            this.fileButton.Size = new System.Drawing.Size(44, 20);
+            this.fileButton.TabIndex = 2;
+            this.fileButton.Text = "...";
+            this.fileButton.UseVisualStyleBackColor = true;
+            this.fileButton.Click += new System.EventHandler(this.fileButton_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(192, 245);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(384, 20);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(149)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(192, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(384, 46);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select RedM Folder Location";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.HelpRequest += new System.EventHandler(this.folderBrowserDialog1_HelpRequest);
+            // 
+            // cacheButton
+            // 
+            this.cacheButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.cacheButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.cacheButton.FlatAppearance.BorderSize = 2;
+            this.cacheButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.cacheButton.Font = new System.Drawing.Font("Arial Narrow", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cacheButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.cacheButton.Location = new System.Drawing.Point(192, 332);
+            this.cacheButton.Name = "cacheButton";
+            this.cacheButton.Size = new System.Drawing.Size(132, 63);
+            this.cacheButton.TabIndex = 7;
+            this.cacheButton.Text = "Clear Cache and Launch";
+            this.cacheButton.UseVisualStyleBackColor = false;
+            this.cacheButton.Click += new System.EventHandler(this.cacheButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(758, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 34);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "X";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 326);
+            this.panel1.TabIndex = 9;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PanelDrag_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.redm;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cacheButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.fileButton);
+            this.Controls.Add(this.LaunchButton);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RedM Cache Cleaner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button LaunchButton;
+        private System.Windows.Forms.Button fileButton;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button cacheButton;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        // Import Win32 APIs
+        [DllImport("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool ReleaseCapture();
+
+        private const int WM_NCLBUTTONDOWN = 0xA1;
+        private const int HT_CAPTION = 0x2;
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Attach to your drag handle (e.g., panelDrag)
+            panel1.MouseDown += PanelDrag_MouseDown;
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string customPath = System.IO.Path.Combine(appDataPath, "RedM");
+            this.textBox1.Text= customPath;
+
+        }
+        private void PanelDrag_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+        }
+
+
+
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileButton_Click(object sender, EventArgs e)
+        {
+            //grab local app data
+            string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            string customPath = System.IO.Path.Combine(appDataPath, "RedM");
+
+            //create folder dialog
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            diag.SelectedPath = customPath; //set the pre folder to the redM location
+
+            if (diag.ShowDialog() == System.Windows.Forms.DialogResult.OK) //open diag and set the text
+            {
+                string folderPath = diag.SelectedPath;
+                this.textBox1.Text = folderPath;
+            }
+            //make an error for selecting the wrong file
+            
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+  
+        }
+
+        private void LaunchButton_Click(object sender, EventArgs e)
+        {
+            string exePath = this.textBox1.Text + "\\RedM.exe";
+            //MessageBox.Show("Selected exe path: " + exePath); test
+            try
+            {
+                Process.Start(exePath);
+                this.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Launching .exe: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+        private void cacheButton_Click(object sender, EventArgs e)
+        {
+            string exePath = this.textBox1.Text + "\\RedM.exe";
+            string cacheGeneral = this.textBox1.Text + "\\RedM.app\\data\\cache";
+            string cacheServer = this.textBox1.Text + "\\RedM.app\\data\\server-cache";
+            string cacheServerPriv = this.textBox1.Text + "\\RedM.app\\data\\server-cache-priv";
+
+            try // delete the directory (folders) and start then close this app
+            {
+
+                Directory.Delete(cacheGeneral, true); // delete cache 1
+                Directory.Delete(cacheServer, true); // delete cache 2
+                Directory.Delete(cacheServerPriv, true); // delete cache 3
+                Process.Start(exePath);
+                this.Close();
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+    }
+}
